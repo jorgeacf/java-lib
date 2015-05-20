3b89f7af70a20465a5d1b6f95cbe56bc26c17973
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2015-05-20 21:33:05 +0100

    Added SearchAlgorithms class

---
+++ src/lib/src/main/java/com/jorgefigueiredo/lib/algorithms/SearchAlgorithms.java
@@ -0,0 +1,16 @@
+package com.jorgefigueiredo.lib.algorithms;
+
+
+
+public class SearchAlgorithms {
+
+	
+	public int BinarySearch(int[] elements, int element) {
+		
+		
+		
+		
+		return -1;
+	}
+	
+}
